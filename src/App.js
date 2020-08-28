ca91bb09eb172131bebf0c289d5f49ccb8cfbdbe
Zikriye Urkmez Cengiz <zikriye.urkmez@navlungo.com>
2020-08-28 07:59:18 +0300

    Solution is added

--- src/App.js
+++ src/App.js
@@ -1,129 +1,132 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Dashboard from "./Dashboard";
 
 /*
-Display a list of movies where each movie contains a list of users that favorited it.
-
+Display a list of movies where each movie contains a list of users that favorited it.ort
 For detailed instructions, refer to instructions.md.
 */
 
 const profiles = [
   {
     id: 1,
-    userID: '1',
-    favoriteMovieID: '1',
+    userID: "1",
+    favoriteMovieID: "1",
   },
   {
     id: 2,
-    userID: '2',
-    favoriteMovieID: '1',
+    userID: "2",
+    favoriteMovieID: "1",
   },
   {
     id: 3,
-    userID: '4',
-    favoriteMovieID: '5',
+    userID: "4",
+    favoriteMovieID: "5",
   },
   {
     id: 4,
-    userID: '5',
-    favoriteMovieID: '2',
+    userID: "5",
+    favoriteMovieID: "2",
   },
   {
     id: 5,
-    userID: '3',
-    favoriteMovieID: '5',
+    userID: "3",
+    favoriteMovieID: "5",
   },
   {
     id: 6,
-    userID: '6',
-    favoriteMovieID: '4',
+    userID: "6",
+    favoriteMovieID: "4",
   },
 ];
 
 const users = {
   1: {
     id: 1,
-    name: 'Jane Jones',
-    userName: 'coder',
+    name: "Jane Jones",
+    userName: "coder",
   },
   2: {
     id: 2,
-    name: 'Matthew Johnson',
-    userName: 'mpage',
+    name: "Matthew Johnson",
+    userName: "mpage",
   },
   3: {
     id: 3,
-    name: 'Autumn Green',
-    userName: 'user123',
+    name: "Autumn Green",
+    userName: "user123",
   },
   4: {
     id: 3,
-    name: 'John Doe',
-    userName: 'user123',
+    name: "John Doe",
+    userName: "user123",
   },
   5: {
     id: 5,
-    name: 'Lauren Carlson',
-    userName: 'user123',
+    name: "Lauren Carlson",
+    userName: "user123",
   },
   6: {
     id: 6,
-    name: 'Nicholas Lain',
-    userName: 'user123',
+    name: "Nicholas Lain",
+    userName: "user123",
   },
 };
 
 const movies = {
   1: {
     id: 1,
-    name: 'Planet Earth',
+    name: "Planet Earth",
   },
   2: {
     id: 2,
-    name: 'Selma',
+    name: "Selma",
   },
   3: {
     id: 3,
-    name: 'Million Dollar Baby',
+    name: "Million Dollar Baby",
   },
   4: {
     id: 4,
-    name: 'Forrest Gump',
+    name: "Forrest Gump",
   },
   5: {
     id: 5,
-    name: 'Get Out',
+    name: "Get Out",
   },
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.usersByMovie = {};
+
+    profiles.forEach((profile) => {
+      const movieID = profile.favoriteMovieID;
+
+      if (this.usersByMovie[movieID]) {
+        this.usersByMovie[movieID].push(profile.userID);
+      } else {
+        this.usersByMovie[movieID] = [profile.userID];
+      }
+    });
+  }
   render() {
     return (
-      {movies.map((movie) => {
-          const favMovieName = movie.name;
-          const favedProfiles = profiles.filter(p=> p.favoriteMovieID === movie.id)
-          favedProfiles.map((fav)=>{
-            const username = users[fav.userID];
-            
-                 return (
-if(!!username) ?
-            {
-            <h2>{favMovieName}</h2>
-            <p>Liked By:</p>
-            <ul>
-              <li>{username}</li>
-            </ul>
-            } : 
-         <h2>{favMovieName}</h2>
-				<p>None of the current users liked this movie</p>
-          );
-            
-          })
-         
-
-     
-        })}
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <h2>How Popular is Your Favorite Movie?</h2>
+        <Dashboard
+          profiles={profiles}
+          movies={movies}
+          users={users}
+          usersByMovie={this.usersByMovie}
+        />
+      </div>
     );
   }
 }
